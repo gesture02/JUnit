eb2f9103130f72eba25d41bf10ff48ddce9e6183
gesture02 <gesture02@gmail.com>
2021-11-30 16:45:40 +0900

    JUnit5 test

--- book/src/test/java/com/cos/book/web/BookControllerIntegreTest.java
+++ book/src/test/java/com/cos/book/web/BookControllerIntegreTest.java
@@ -1,11 +1,36 @@
 package com.cos.book.web;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.cos.book.Service.BookService;
+import com.cos.book.domain.Book;
+import com.cos.book.domain.BookRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 1. 통합 테스트 (모든 bean을 IoC에 올리고 테스트)
@@ -14,6 +39,8 @@ import org.springframework.transaction.annotation.Transactional;
  * AutoConfigureMockMvc : MockMvc IoC에 등록
  * Transactional : 각각의 테스트 함수가 종료될 때마다 트랜잭션을 rollback해주는 역할
  */
+
+@Slf4j
 @Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
@@ -21,6 +48,59 @@ public class BookControllerIntegreTest {
 
 	@Autowired
 	private MockMvc mockMvc; // 얘를 통해 테스트
+	@Autowired
+	private BookRepository bookRepository;
+	@Autowired
+	private EntityManager entityManager;
 	
+	@BeforeEach//실행 전에 무조건 실행되는 것, 여기서 시행되기 전에 서로 독립적으로 만들어주면된다.
+	public void init() {//ALTER TABLE book AUTO_INCREMENT = 1
+		entityManager.createNativeQuery("ALTER TABLE book ALTER COLUMN id RESTART WITH 1").executeUpdate();
+	}
 	
+	// BDDMockito 패턴 : given, when, then
+	@Test
+	public void save_test() throws Exception {
+		log.info("save_test() 시작 =========================");
+		Book book = new Book(null, "spring", "cos");
+		
+		//given (테스트를 하기 위한 준비)
+		String content = new ObjectMapper().writeValueAsString(book); // object to json
+		
+		
+		//when (테스트 실행)
+		ResultActions resultAction = mockMvc.perform(post("/book")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(content)
+				.accept(MediaType.APPLICATION_JSON_UTF8)
+				);
+		
+		//then (검증)
+		resultAction
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.title").value("spring"))
+			.andDo(MockMvcResultHandlers.print());
+		
+	}
+	
+	@Test
+	public void findAll_test() throws Exception {
+		//given
+		List<Book> books = new ArrayList<>();
+		books.add(new Book(null, "spring", "cos"));
+		books.add(new Book(null, "react", "cos"));
+		bookRepository.saveAll(books);
+		
+		//when
+		ResultActions resultAction = mockMvc.perform(get("/book")
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$", Matchers.hasSize(2)))
+			.andExpect(jsonPath("$.[0].title").value("spring"))
+			.andDo(MockMvcResultHandlers.print());
+			
+	}
 }
--- book/src/test/java/com/cos/book/web/BookControllerUnitTest.java
+++ book/src/test/java/com/cos/book/web/BookControllerUnitTest.java
@@ -1,10 +1,108 @@
 package com.cos.book.web;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import com.cos.book.Service.BookService;
+import com.cos.book.domain.Book;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 
 //2. 유닛 테스트(Controller 관련 로직만 띄우기 : Filter, ControllerAdvice 등)
+
+@Slf4j
 @WebMvcTest //JUnit5에서는 WebMvcTest안에 @ExtendWith(SptringExtension.class) 포함 //이 파일을 스프링 환경으로 확장하고싶을 때 붙임
 public class BookControllerUnitTest {
-
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean // BookService가 IoC 환경에 등록됨 (가짜 service : 진짜를 쓰면 repository랑 다 연결되야함)
+	private BookService bookService;
+	
+	// BDDMockito 패턴 : given, when, then
+	@Test
+	public void save_test() throws Exception {
+		log.info("save_test() 시작 =========================");
+		Book book = new Book(null, "spring", "cos");
+		
+		//given (테스트를 하기 위한 준비)
+		String content = new ObjectMapper().writeValueAsString(book); // object to json
+		when(bookService.save(book)).thenReturn(new Book(1L, "spring", "cos"));
+		
+		//when (테스트 실행)
+		ResultActions resultAction = mockMvc.perform(post("/book")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(content)
+				.accept(MediaType.APPLICATION_JSON_UTF8)
+				);
+		
+		//then (검증)
+		resultAction
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.title").value("spring"))
+			.andDo(MockMvcResultHandlers.print());
+		
+	}
+	
+	@Test
+	public void findAll_test() throws Exception {
+		//given
+		
+		List<Book> books = new ArrayList<>();
+		books.add(new Book(1L, "spring", "cos"));
+		books.add(new Book(2L, "react", "cos"));
+		
+		when(bookService.findAll()).thenReturn(books);
+		
+		//when
+		ResultActions resultAction = mockMvc.perform(get("/book")
+				.accept(MediaType.APPLICATION_JSON_UTF8)
+				);
+		
+		//then
+		resultAction
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$", Matchers.hasSize(2)))
+			.andExpect(jsonPath("$.[0].title").value("spring"))
+			.andDo(MockMvcResultHandlers.print());
+			
+	}
+	
+	@Test
+	public void findById_test() throws Exception {
+		// given
+		Long id = 1L;
+		when(bookService.findById(id)).thenReturn(new Book(1L, "spring", "cos"));
+		
+		// when
+		ResultActions resultAction = mockMvc.perform(get("/book/{id}", id)
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		// then
+		resultAction
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.title").value("spring"))
+			.andDo(MockMvcResultHandlers.print());
+	}
 }
