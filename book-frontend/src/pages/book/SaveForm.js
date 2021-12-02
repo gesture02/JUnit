1263b20af2df7062481fc4766123ff8f352919ad
gesture02 <gesture02@gmail.com>
2021-12-02 21:38:21 +0900

    글목록보기, 글쓰기

--- book-frontend/src/pages/book/SaveForm.js
+++ book-frontend/src/pages/book/SaveForm.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
-const SaveForm = () => {
+const SaveForm = (props) => {
+  const [book, setBook] = useState({ title: '', author: '' });
+  const changeValue = (e) => {
+    setBook({ ...book, [e.target.name]: e.target.value });
+  };
+
+  const submitBook = (e) => {
+    e.preventDefault(); // submit이 action을 안타고 자기 할일을 그만함.
+    fetch('http://localhost:8080/book', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      body: JSON.stringify(book),
+    })
+      .then((res) => {
+        console.log(1, res);
+        if (res.status === 201) {
+          return res.json();
+        } else {
+          return null;
+        }
+      })
+      .then((res) => {
+        if (res !== null) {
+          props.history.push('/');
+        } else {
+          alert('책 등록에 실패했습니다');
+        }
+      });
+  };
+
   return (
-    <div>
-      <h1>책 등록하기</h1>
-    </div>
+    <Form onSubmit={submitBook}>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter title"
+          onChange={changeValue}
+          name="title"
+        />
+      </Form.Group>
+      <br />
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Author"
+          onChange={changeValue}
+          name="author"
+        />
+      </Form.Group>
+      <br />
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
